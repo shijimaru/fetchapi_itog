c4e22f3b3048b94830626d136c9d53a5a3d783a6
shijimaru <124866373+shijimaru@users.noreply.github.com>
2024-10-28 23:41:39 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const weathere = document.getElementById('weather')
+
+async function getWeather() {
+    const latitude = 55.7558; // Широта Москвы
+    const longitude = 37.6173; // Долгота Москвы
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
+    const data = await response.json();
+    const weather = data.current_weather;
+
+    weathere.innerHTML = `
+        <h2>Текущая погода в Москве</h2>
+        <p>Температура: ${weather.temperature}°C</p>
+        <p>Скорость ветра: ${weather.windspeed} м/с</p>
+        <p>Направление ветра: ${weather.winddirection}°</p>
+    `;
+}
+
+window.onload = getWeather;
